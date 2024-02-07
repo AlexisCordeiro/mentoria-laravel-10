cfb3e8d7c600695b2597c0d4f150214ad5c095be
Aléxis Abner Cordeiro da Silva <alexiscordeiro2210@gmail.com>
2024-02-07 14:39:48 -0300

    front-end

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,39 @@
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu"
+        aria-labelledby="sidebarMenuLabel">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                    <svg class="bi">
+                        <use xlink:href="#house-fill" />
+                    </svg>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <svg class="bi">
+                        <use xlink:href="#file-earmark" />
+                    </svg>
+                    Vendas
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <svg class="bi">
+                        <use xlink:href="#cart" />
+                    </svg>
+                    Produtos
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <svg class="bi">
+                        <use xlink:href="#people" />
+                    </svg>
+                    Clientes
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
